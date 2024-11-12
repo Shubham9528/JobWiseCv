7d87a3c59c02c567387f2799387c43b599c50912
Shubham9528 <81708211+Shubham9528@users.noreply.github.com>
2024-11-12 15:50:14 +0530

    Update(AiResume textbox width)

--- frontend/src/components/AiResume.jsx
+++ frontend/src/components/AiResume.jsx
@@ -20,12 +20,12 @@ function AiResume() {
 
     return (
         
-    <div className="flex flex-col items-center p-8 bg-gradient-to-br from-gray-900 via-gray-800 to-gray-700 min-h-screen text-gray-100 font-sans">
+    <div className="flex flex-col items-center p-3 bg-gradient-to-br from-gray-900 via-gray-800 to-gray-700 min-h-screen text-gray-100 font-sans">
   
-  <h1 className="text-4xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-indigo-400 via-purple-500 to-pink-500 animate-pulse mb-6">
+  <h1 className="text-4xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-indigo-400 via-purple-500 to-pink-500 animate-pulse mb-4">
     AI Generated Resume
   </h1>
-  <h3 className="text-xl text-gray-200">
+  <h3 className="text-xl text-gray-200 mb-3">
   Update your resume accordingly and check your ATS score{"  "} 
   <a 
     href="https://skillsyncer.com/" 
@@ -42,7 +42,7 @@ function AiResume() {
     cols="50"
     value={aiResponse || "AI in progress, Please wait..."}
     readOnly
-    className="w-full max-w-4xl p-4 text-lg rounded-lg bg-gray-900 text-gray-100 border border-gray-600 shadow-xl resize-none focus:outline-none focus:ring-4 focus:ring-indigo-500 transition duration-300 scrollbar-thin scrollbar-thumb-indigo-500 scrollbar-track-gray-700 hover:scrollbar-thumb-indigo-400"
+    className="min-w-full min-h-full p-4 text-lg rounded-lg bg-gray-900 text-gray-100 border border-gray-600 shadow-xl resize-none focus:outline-none focus:ring-4 focus:ring-indigo-500 transition duration-300 scrollbar-thin scrollbar-thumb-indigo-500 scrollbar-track-gray-700 hover:scrollbar-thumb-indigo-400"
   />
 </div>);
 }
